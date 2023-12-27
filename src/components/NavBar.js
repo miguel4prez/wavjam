f8e9f270c326d81d1c90306216269aa64b8a7fc5
Miguel <miguelvasquez2201@gmail.com>
2023-12-27 12:16:37 -0500

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
+
+export default function NavBar(){
+  return (
+    <>
+      <img />
+      <input type="text" placeholder="Search for... 'lil uzi'"/>
+      <CloudUploadOutlinedIcon />
+      <NotificationsActiveOutlinedIcon />
+      <a />
+    </>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import '../styles/home.css'
+import 'semantic-ui-css/semantic.min.css'
+
+function Home(){
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default Home
